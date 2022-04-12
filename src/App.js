93c83f62d2de9d4c1ea71dccf95a8b36ab3bc7bd
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-12 23:46:09 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Header from './components/Header/Header';
 import Home from './Pages/Home/Home/Home';
 import Contact from './Pages/Contact/Contact';
 import About from './Pages/About/About';
-import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
+import Login from './Pages/Login/Login';
 import Footer from './Pages/Footer/Footer';
 import NotFound from './components/NotFound/NotFound';
 
@@ -24,8 +24,8 @@ const App = () => {
                 <Route path='/home' element={<Home />}></Route>
                 <Route path='/contact' element={<Contact />}></Route>
                 <Route path='/about' element={<About />}></Route>
-                <Route path='/login' element={<Login />}></Route>
                 <Route path='/signup' element={<Signup />}></Route>
+                <Route path='/login' element={<Login />}></Route>
                 <Route path='*' element={<NotFound></NotFound>}></Route>
             </Routes>
             <Footer></Footer>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './Header.css'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import logo from '../../images/logo.png'
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
+
     return (
-        <div className='text-center py-5'>
-            <nav>
-                <ul>
-                    <Link to='/'> Home </Link>
-                    <Link to='/about'> About </Link>
-                    <Link to='/contact'> Contact </Link>
-                    <Link to='/login'> Login </Link>
-                    <Link to='/signup'> Register </Link>
-                </ul>
-            </nav>
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src={logo} height="40" alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav className='nav-style'>
+                            <CustomLink to='/'>Home</CustomLink>
+                            <CustomLink to='/services'>Services</CustomLink>
+                            <CustomLink to='/about'>About </CustomLink>
+                            <CustomLink to='/contact'>Contact</CustomLink>
+                            <CustomLink to='/signup'>Sign up</CustomLink>
+                            <CustomLink to='/login'>Log in</CustomLink>
+                            {/* <NavDropdown title="Our Sponsor" id="collasible-nav-dropdown">
+                                <CustomLink className='text-danger' to='/emajhon'>Ema John </CustomLink>
+                                <CustomLink className='text-danger' to='/model'>Model</CustomLink>
+                                <CustomLink className='text-danger' to='/Scncs'>Scncs</CustomLink>
+                            </NavDropdown> */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
