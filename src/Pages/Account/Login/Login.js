326acb57f2c0ae1d68a44d6ac36fe41468279101
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-13 20:30:29 +0600

    signup section ui done

--- src/Pages/Account/Login/Login.js
+++ src/Pages/Account/Login/Login.js
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import './Login.css'
 const Login = () => {
+    const navigate = useNavigate()
+    const emailRef = useRef('')
+    const passwordRef = useRef('')
+
+
+    //handle log in
+    const handleLogin = e => {
+        e.preventDefault()
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+    }
+
+
     return (
         <div className='bg-img'>
-            <div className="row container">
-                <div className="col-md-6  py-5">
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <div className="row container py-5 mx-2">
+                <div className="col-md-6 text-light custom-style ">
+                    <h3 className='text-center pb-3'>Hello There,</h3>
+                    <h5 className='text-center pb-3'>Welcome Back</h5>
+
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group className="mb-3 " controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
+                            <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Check me out" />
                         </Form.Group>
                         <Button variant="primary" type="submit">
-                            Submit
+                            Login
                         </Button>
                     </Form>
+                    <p className='toggle-page'>
+                        New to Genius Car Service ?{" "}
+                        <span onClick={() => navigate("/signup")}>Create New Account</span>
+                    </p>
                 </div>
             </div>
         </div>
--- src/Pages/Account/Signup/Signup.js
+++ src/Pages/Account/Signup/Signup.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import './Signup.css'
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const navigate = useNavigate()
     return (
-        <div>
-            <h2>This is sign up page</h2>
+        <div className='bg-img2'>
+            <div className="row container py-5 mx-2">
+                <div className="col-md-6  text-light custom-style ">
+                    <h3 className='text-center pb-3'>Hello There,</h3>
+                    <h5 className='text-center pb-3'>Register now to explore more</h5>
+
+                    <Form >
+                        <Form.Group className="mb-3 " controlId="formBasicUsername">
+                            <Form.Label>Your User Name</Form.Label>
+                            <Form.Control type="name" placeholder="Enter name" required />
+
+                        </Form.Group>
+
+                        <Form.Group className="mb-3 " controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" required />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" required />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+                    <p className='toggle-page'>
+                        Already have an account  ?{" "}
+                        <span onClick={() => navigate("/login")}>Please Login</span>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
