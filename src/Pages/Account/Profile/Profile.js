c555e8e9ba1d026a627974648d3f4f03a70bb466
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-14 18:03:19 +0600

    profile name update

--- src/Pages/Account/Profile/Profile.js
+++ src/Pages/Account/Profile/Profile.js
@@ -8,7 +8,7 @@ const Profile = () => {
     return (
         <div className='my-5 text-center'>
             <h2>Welcome To your profile</h2>
-            <h4>Your Name : <span className='text-danger'> {user?.displayName ? "user?.displayName" : "Not Update"}</span> </h4>
+            <h4>Your Name : <span className='text-danger'> {user?.displayName ? user?.displayName : "Not Update"}</span> </h4>
             <h5>Your email is : {user?.email}</h5>
         </div>
     );
